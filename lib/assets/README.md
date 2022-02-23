84c626489b691b16167d7367209a7f8aef596e28
btwq97 <benjamin.tan@outlook.sg>
2022-02-23 21:59:49 +0800

    updated .gitignore

---
+++ lib/assets/README.md
@@ -0,0 +1,11 @@
+Insert an .env file as follows:
+
+```
+# DynamoDB
+awsRegion=""
+awsEndPoint=""
+# MQTT
+mqttHost="localhost"
+mqttUsername=""
+mqttPassword=""
+```
--- lib/uipagecustomer/canteen_map.dart
+++ lib/uipagecustomer/canteen_map.dart
@@ -1,16 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../constants.dart';
-import 'package:flutter_blue/flutter_blue.dart';
-import 'package:sensors_plus/sensors_plus.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'dart:isolate';
-import 'package:on_sight/connectivity/bluetooth_main.dart';
-import 'package:on_sight/connectivity/bluetooth_widgets.dart';
-
-import 'dart:async';
-import 'dart:io';
-import 'dart:math' as math;
 
 class CanteenTestPage extends StatefulWidget {
   @override
@@ -24,8 +12,7 @@ class _CanteenTestPageState extends State<CanteenTestPage> {
           appBar: AppBar(title: Text('Canteen Map')),
           body: InteractiveViewer(
               child: Center(
-                  child: Image.asset('images/Technoedge_Map_PurpleBG.png'))
-          )),
+                  child: Image.asset('images/Technoedge_Map_PurpleBG.png')))),
     );
   }
 }
